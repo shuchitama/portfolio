f4e37a3e1c844e84e475cd5992271400a8664470
Shuchita <shuchita@live.com>
2021-08-06 18:04:53 -0700

    import Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 
+//Importing components
+import Form from "./components/Form";
+
 function App() {
 	return (
 		<div className='App'>
 			<header>
 				<h1>To Do List</h1>
 			</header>
+
+			<Form />
 		</div>
 	);
 }
