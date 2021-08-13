3eaa11e651d8d35290b5d94afbefc0b99b797714
Shuchita <shuchita@live.com>
2021-08-12 22:42:44 -0700

    update state with todo from inputtext

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import TodoList from "./components/TodoList";
 
 function App() {
 	const [inputText, setInputText] = useState("");
+	const [todos, setTodos] = useState([]);
 	return (
 		<div className='App'>
 			<header>
 				<h1>To Do List</h1>
 			</header>
 
-			<Form setInputText={setInputText} />
+			<Form
+				inputText={inputText}
+				setInputText={setInputText}
+				todo={todos}
+				setTodos={setTodos}
+			/>
 			<TodoList />
 			{inputText}
 		</div>
